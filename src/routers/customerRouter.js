7d5203b46aff057d4bc1d914d18cd5986f498ac5
Albert Felix <albertfelixleo@gmail.com>
2021-02-08 13:39:22 +0530

    customerRouter updates based on customer id

--- src/routers/customerRouter.js
+++ src/routers/customerRouter.js
@@ -20,12 +20,18 @@ customerRouter
 })
 .post("/add", (req,res) => {
     const customer = req.body;
+    customer[id] = CustomerList.length + 1
     CustomerList.push(customer)
     res.status(200).json({status: "SUCCESS"})
 })
 .post("/edit/:id", (req,res) => {
     const customer = req.body;
-    CustomerList.splice(req.params.id, 1, customer)
+    for(let i=0; i<CustomerList.length; i++){
+        if(CustomerList[i]['id'] == req.params.id){
+            CustomerList.splice(i, 1, customer)
+            break
+        }
+    }
     res.status(200).json({status: "SUCCESS"})
 })
 
